1075b775ecbeeae538c5d654e3f2ab1270aa6500
Ruslan Timershin <metwisom@gmail.com>
2023-02-10 13:43:46 +0300

    refactor: add keyboard key layer

--- src/classes/Engine/GameKeyboard.ts
+++ src/classes/Engine/GameKeyboard.ts
@@ -1,11 +1,13 @@
 import {Entity} from "../Entity";
 import {_Keyboard} from "./Keyboard";
+import {KeyboardKey} from "./KeyboardKey";
+
 
 class _GameKeyboard {
 
-  private a = false;
-  private d = false;
-  private space = false;
+  private a = new KeyboardKey();
+  private d = new KeyboardKey();
+  private space = new KeyboardKey();
 
   private slave: Entity;
   private master: _Keyboard;
@@ -27,26 +29,26 @@ class _GameKeyboard {
   // }
 
   updateState() {
-    this.a = this.master.getKey("KeyA") !== undefined;
-    this.d = this.master.getKey("KeyD") !== undefined;
-    this.space = this.master.getKey("Space") !== undefined;
+    this.a.set(this.master.getKey("KeyA") !== undefined);
+    this.d.set(this.master.getKey("KeyD") !== undefined);
+    this.space.set(this.master.getKey("Space") !== undefined);
   }
 
   update() {
     if (this.master === undefined || this.slave === undefined) {
       return;
     }
-    if (this.a && this.slave.hasGround) {
+    if (this.a.status && this.slave.hasGround) {
       this.slave.faced = "left";
       this.slave.momentum = -this.slave.speed;
     }
 
-    if (this.d && this.slave.hasGround) {
+    if (this.d.status && this.slave.hasGround) {
       this.slave.faced = "right";
       this.slave.momentum = this.slave.speed;
     }
 
-    if (this.space && this.slave.hasGround) {
+    if (this.space.pressed && this.slave.hasGround) {
       this.slave.eDown = -11;
       this.slave.hasGround = false;
       this.slave.state = "jump";
