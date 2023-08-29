f718905dcbac3930d8f5c88722fbffe4a6033c13
Ruslan Timershin <metwisom@gmail.com>
2023-08-30 01:04:42 +0300

    feature: revert randFunc

--- src/utils/getRandom.ts
+++ src/utils/getRandom.ts
@@ -1,9 +1,15 @@
 
-// const getRandom = (min: number, max: number): number => {
-//   const minInt = Math.ceil(min);
-//   const maxInt = Math.floor(max);
-//
-//   return Math.floor(Math.random() * maxInt + 1) + minInt;
-// };
+const getRandom = (min: number, max: number): number => {
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max) -  Math.ceil(min);
 
-// export {getRandom};
+  return Math.floor(Math.random() * maxInt + 1) + minInt;
+};
+const getRandomFloat = (min: number, max: number): number => {
+  const minInt = min;
+  const maxInt = max -  min;
+
+  return Math.random() * maxInt + minInt;
+};
+
+export {getRandom,getRandomFloat};
