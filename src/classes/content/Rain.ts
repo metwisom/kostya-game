56b3ca7ac5f020d98787f68754750d909dd149c9
Ruslan <metwisom@gmail.com>
2024-02-03 03:22:40 +0300

    fix Rain.ts

--- src/classes/content/Rain.ts
+++ src/classes/content/Rain.ts
@@ -3,13 +3,14 @@ import {Engine} from '../Engine/Engine';
 import {Camera} from '../Engine/Camera';
 import {TextureRain} from '../Texture/TextureRain';
 import {Box} from '../Box/Box';
-import {Gravitational} from '../Effector/Gravity';
-import {D2Updatable} from '../D2Updatable';
+import {Gravitational, Gravity} from '../Effector/Gravity';
+import {BoxTextured} from '../Box/BoxTextured';
+import {ItemWithStates} from '../ItemWithStates';
 
 const texture = new TextureRain(5, 11);
 texture.setColor('#65ada0');
 
-class Rain extends D2Updatable implements Gravitational{
+class Rain extends ItemWithStates implements Gravitational{
 
   public eDown = 1;
   public hasGround = false
@@ -25,17 +26,17 @@ class Rain extends D2Updatable implements Gravitational{
 
     super();
 
-   // this._effector.addEffect(new Gravity(this));
+   this._effector.addEffect(new Gravity(this));
 
     this.hasCollision = false;
     this.maxDepth = maxDepth;
 
-     // this._physBox = new Box(4, 8, 5, 11, this);
-    // this.physBox.hasCollision = false;
+     this._physBox = new Box(4, 8, 5, 11, this);
+    this.physBox.hasCollision = false;
 
-    // this.viewBox = new BoxTextured(0, 0, 5, 11, this, texture);
+    this.viewBox = new BoxTextured(0, 0, 5, 11, this, texture);
 
-    // this.respawn();
+    this.respawn();
 
   }
 
