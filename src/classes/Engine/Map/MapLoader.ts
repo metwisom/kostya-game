a755a25c6d5744632865443a1e0c7157c773c1b4
Ruslan <metwisom@gmail.com>
2024-02-04 00:32:47 +0300

    fix Parallax

--- src/classes/Engine/Map/MapLoader.ts
+++ src/classes/Engine/Map/MapLoader.ts
@@ -1,13 +1,13 @@
-import fetch from "node-fetch";
-import {BackgroundEntity, GameMap, MapEntity} from "./iMap";
-import {Structure} from "../../content/Structure";
-import {Engine} from "../Engine";
-import {Parallax} from "../../content/Parallax";
-import {D2Drawable} from "../../D2Drawable";
-import {D2Updatable} from "../../D2Updatable";
-import {Character} from "../../content/Character";
-import {Camera} from "../Camera";
-import {InputController} from "../Input/InputController";
+import fetch from 'node-fetch';
+import {BackgroundEntity, GameMap, MapEntity} from './iMap';
+import {Structure} from '../../content/Structure';
+import {Engine} from '../Engine';
+import {Parallax} from '../../content/Parallax';
+import {D2Drawable} from '../../D2Drawable';
+import {D2Updatable} from '../../D2Updatable';
+import {Character} from '../../content/Character';
+import {Camera} from '../Camera';
+import {InputController} from '../Input/InputController';
 
 class _MapLoader {
 
@@ -19,44 +19,49 @@ class _MapLoader {
 
 
     await fetch(resourceMap)
-      .then(res => res.json())
-      .then((data: GameMap) => {
-        data.map.map((item) =>
-          readyMapList.push(item)
-        );
-        data.background.items.map((item) =>
-          readyParallaxList.push(item)
-        );
-        const Kostya = new Character(data.spawnPoint.x, data.spawnPoint.y);
-        Engine.addObject(Kostya, 2);
-        Engine.addObjectPhys(Kostya);
-        InputController.setSlave(Kostya);
-        Camera.attach(Kostya);
-      });
+    .then(res => res.json())
+    .then((data: GameMap) => {
+      data.map.map((item) =>
+        readyMapList.push(item),
+      );
+      data.background.items.map((item) =>
+        readyParallaxList.push(item),
+      );
+      const Kostya = new Character(data.spawnPoint.x, data.spawnPoint.y);
+      Engine.addObject(Kostya, 2);
+      Engine.addObjectPhys(Kostya);
+      InputController.setSlave(Kostya);
+      Camera.attach(Kostya);
+    });
 
     readyMapList.map(item => {
-      let someObject:D2Updatable = undefined;
+      let someObject: D2Updatable = undefined;
       switch (item.type) {
-      case "ground":
-        someObject = new Structure(item.x, item.y);
-        break;
+        case 'ground':
+          someObject = new Structure(item.x, item.y);
+          break;
       }
       Engine.addObject(someObject, 1);
       Engine.addObjectPhys(someObject);
       this.set(item.x, item.y, someObject);
-      return someObject
+      return someObject;
     }).map(i => {
-      if(i instanceof Structure){
-        i.refreshSprite()
+      if (i instanceof Structure) {
+        i.refreshSprite();
       }
     });
 
     readyParallaxList.map(item => {
+      const plx0 = new Parallax(item.image, item.bias);
+      plx0.setOriginX(-plx0.draw().width);
+      Engine.addObject(plx0, 0);
+
       const plx1 = new Parallax(item.image, item.bias);
-      Engine.addObject(plx1,0);
-        const plx2 = new Parallax(item.image, item.bias);
-        plx2.setOriginX(plx1.draw().width)
-        Engine.addObject(plx2,0);
+      Engine.addObject(plx1, 0);
+
+      const plx2 = new Parallax(item.image, item.bias);
+      plx2.setOriginX(plx1.draw().width);
+      Engine.addObject(plx2, 0);
     });
     // this.refreshTextures();
   }
--- src/classes/content/Parallax.ts
+++ src/classes/content/Parallax.ts
@@ -25,7 +25,6 @@ class Parallax extends D2Updatable {
     const width = texture.referenceImage.width * ratio;
     const height = texture.referenceImage.height * ratio;
 
-    console.log(height);
 
     this.viewBox = new BoxTextured(0, 0, width, height, this);
     this.viewBox.texture = texture;
@@ -40,9 +39,12 @@ class Parallax extends D2Updatable {
   draw(): ViewArea {
     this.y = -Camera.y - Engine.display.height / 2;
 
-    this.x = this.viewBox.width * Math.floor((Camera.x - this.viewBox.width / 2) / this.viewBox.width);
 
-    this.x = this.x + this._originX;
+    this.x = this.viewBox.width * Math.floor(Camera.x / this.viewBox.width);
+
+     this.x = this.x +  ( Camera.x * this.bias / 20  ) % this.viewBox.width;
+
+    this.x = (this.x + this._originX) ;
     this.y = Camera.y - Engine.display.height / 2 + Camera.target.viewBox.height / 2;
 
     return super.draw();
