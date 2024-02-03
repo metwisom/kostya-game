dfd703ee7cf2e8a147c600e5bcde25e8bddc42be
Ruslan <metwisom@gmail.com>
2024-02-04 00:39:10 +0300

    chore(map): remove bias params

--- src/classes/Engine/Map/iMap.ts
+++ src/classes/Engine/Map/iMap.ts
@@ -1,20 +1,22 @@
 export interface GameMap {
   mapName: string;
-  spawnPoint:{
-    x:number,
-    y:number
-  }
+  spawnPoint: {
+    x: number,
+    y: number
+  };
   background: Background;
   map: (MapEntity)[];
 }
+
 export interface Background {
   type: string;
   items?: (BackgroundEntity)[] | null;
 }
+
 export interface BackgroundEntity {
   image: string;
-  bias: number;
 }
+
 export interface MapEntity {
   x: number;
   y: number;
