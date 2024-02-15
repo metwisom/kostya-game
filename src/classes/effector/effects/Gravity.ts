a516ac256f2cb0d263bb5d79320f66ba2f469d70
Ruslan <metwisom@gmail.com>
2024-02-15 16:10:05 +0300

    refactor(Gravity)

--- src/classes/effector/effects/Gravity.ts
+++ src/classes/effector/effects/Gravity.ts
@@ -3,53 +3,46 @@ import {ItemWithStates} from '../../Engine/ItemWithStates';
 import {Engine} from '../../Engine/Engine';
 import {IEffect} from '../IEffect';
 
-
 interface Gravitational {
   hasGround: boolean;
   eDown: number;
 }
 
 class Gravity extends IEffect {
-
-  protected readonly maintainer: ItemWithStates & Gravitational;
-
-  constructor(maintainer: typeof Gravity.prototype.maintainer) {
+  constructor(protected readonly maintainer: ItemWithStates & Gravitational) {
     super(maintainer);
   }
 
   update(delta: number = 1) {
-
+    const {physBox, id} = this.maintainer;
     let inter: D2Updatable[] = [];
 
     this.maintainer.eDown += 0.1;
-    const yCollision = this.maintainer.physBox.prop(0, this.maintainer.eDown * delta);
+    const yCollision = physBox.prop(0, this.maintainer.eDown * delta);
 
-    if (this.maintainer.physBox.hasCollision) {
-      inter = Engine.checkCollision(yCollision, this.maintainer.id);
+    if (physBox.hasCollision) {
+      inter = Engine.checkCollision(yCollision, id);
     }
+
     if (inter.length === 0) {
       this.maintainer.y += this.maintainer.eDown * delta;
       this.maintainer.hasGround = false;
     } else {
-      if (this.maintainer.y != inter[0].y) {
-        this.maintainer.y = inter[0].y;
+      const {y} = inter[0];
+      if (this.maintainer.y !== y) {
+        this.maintainer.y = y;
         this.maintainer.hasGround = true;
       }
       this.maintainer.eDown = 0;
     }
 
-
     if (this.maintainer.eDown > 0) {
       this.maintainer.state = 'jump';
     } else if (this.maintainer.eDown < 0) {
       this.maintainer.state = 'fall';
-
-    } else {
-      if (this.maintainer.state == 'jump' || this.maintainer.state == 'fall')
+    } else if (this.maintainer.state === 'jump' || this.maintainer.state === 'fall') {
       this.maintainer.state = 'idle';
     }
-
-
   }
 }
 
